a8e7347498feb7b9fe6468d27367250fcf154088
Onur <yildirimonur89@hotmail.com>
2021-04-13 18:11:12 +0300

    Create client api and report grpc service

---
+++ src/PhoneDirectory/ReportService/Services/ReportService.cs
@@ -0,0 +1,26 @@
+﻿using Grpc.Core;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ReportService.Services
+{
+    public class ReportService : Greeter.GreeterBase
+    {
+        private readonly ILogger<ReportService> _logger;
+        public ReportService(ILogger<ReportService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
